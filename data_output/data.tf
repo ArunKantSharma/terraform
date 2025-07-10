348a0322a638fd425625cf4295b756823b193a19
ArunKantSharma <40794377+ArunKantSharma@users.noreply.github.com>
2025-07-10 11:59:45 +0530

    Data output

---
+++ data_output/data.tf
@@ -0,0 +1,21 @@
+#Retrieve the list of AZs in the current AWS region
+data "aws_availability_zones" "available" {}
+data "aws_region" "current" {}
+
+# Terraform Data Block - Lookup Ubuntu 20.04
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
